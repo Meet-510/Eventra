6db378259081e1b2c27574b05748cea21d576e0a
Meet-510 <mistrymeet941@gmail.com>
2026-04-07 20:03:41 -0600

    Eventra got some backend power

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,8 +2,15 @@ const OTP = require('../models/OTP');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { sendOTPEmail } = require('../utils/email');
+const jwt = require('jsonwebtoken');
 
 //Register user
+
+const generatetoken = (id, role)=>{
+    return jwt.sign({id,role}),
+    process.env.JWT_SECRET,
+    {expiresIn:'7d'}
+}
 
 exports.registerUser = async (req,res) => {
     const {name, email, password} = req.body;
@@ -39,4 +46,54 @@ exports.registerUser = async (req,res) => {
     }catch(error){
         res.status(400).json({error:error.message});
     }
-}
+};
+
+//login user
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    let user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({error:"Invalid credentials"});
+
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({error:'Invalid credentials'});
+    }
+    if(!user.isVerified && user.role === 'user'){
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        await OTP.deleteMany({email,otp,action:'account_verification'});
+        await OTP.create({email,otp,action:"account_verification"});
+        await sendOTPEmail(email,otp,'account_verification');
+        return res.status(400).json({
+            error:'Account not verified. A new OTP has been sent to your email.'
+        });
+    }
+
+    res.json({
+        message: "Login Successful",
+        _id : user._id,
+        name: user.name,
+        email:user.email,
+        role:user.role,
+        token: generatetoken(user._id, user.role),
+
+
+    });
+
+    //verify OTP
+    exports.verifyOtp = async (req,res) =>{
+        const {email, otp}= req.body;
+        const otpRecord = await OTP.findOne({email,otp, action:'account_verification'});
+
+        if(! otpRecord){
+            return res.status(400).json({error:"Invalid or expired OTP"});
+
+        }
+        await User.findOneAndUpdate({email}, {isVerified: true});
+        await OTP.deleteMany({email, action:'account_verification'});
+        res.json({message:"Account verified successfully. You can login now"});
+    
+};
+ 
