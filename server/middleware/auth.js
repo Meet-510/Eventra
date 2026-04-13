a0692e864cd646d730739811b3921a0aca73a49a
Meet-510 <mistrymeet941@gmail.com>
2026-04-13 16:48:05 -0600

    Backend

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,7 +8,10 @@ const protect = async ( req, res, next)=>{
         try{
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
-            
+            if(!req.user){
+                return res.status(401).json({message: "Not authorized"})
+            }
+            next();
 
         }catch(error){
             return res.status(401).json({message:"Not authorized, token failed"})
@@ -17,4 +20,15 @@ const protect = async ( req, res, next)=>{
     else{
         return res.status(401).json({message: "Not authorized, No token"});
     }
+};
+
+const admin = (req, res, next) =>{
+    if(req.user && req.user.role === 'admin'){
+        next();
+    }
+    else{
+        return res.status(403).json({message: 'Forbidden, admin access required'})
+    }
 }
+
+module.exports = { protect, admin };
